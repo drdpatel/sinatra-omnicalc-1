b113ca779ac9896e511a7be61c81d608bfe49642
drdpatel <165212603+drdpatel@users.noreply.github.com>
2024-04-25 02:36:49 +0000

    hello

--- app.rb
+++ app.rb
@@ -1,12 +1,17 @@
 require "sinatra"
 require "sinatra/reloader"
 
+get("/howdy") do
+erb(:hello)
+end
 
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
   "
+  
+
 end
 
 get("/square/new") do
@@ -26,4 +31,3 @@ end
 get("/payment/new") do
 
 end
-
