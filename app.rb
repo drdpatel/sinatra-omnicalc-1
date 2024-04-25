f22276b3b52191a6f6d77aff7de6311338e944f1
drdpatel <165212603+drdpatel@users.noreply.github.com>
2024-04-25 01:56:23 +0000

    framework

--- app.rb
+++ app.rb
@@ -1,9 +1,29 @@
 require "sinatra"
 require "sinatra/reloader"
 
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/square/new") do
+
+end
+
+get("square-root/new") do
+
+
+end
+
+get("random/new") do
+
+end
+
+
+get("/payment/new") do
+
+end
+
