b8e6690f99a8a9743cf0cf4c5495e2249dbb3622
drdpatel <165212603+drdpatel@users.noreply.github.com>
2024-04-25 03:48:30 +0000

    night

--- app.rb
+++ app.rb
@@ -5,18 +5,6 @@ get("/howdy") do
 erb(:hello)
 end
 
-get("/results") do
-  erb(:results)
-end
-
-get("/square/results") do
-  @the_num = params.fetch("square_with_form").to_f
-
-  @the_result = params.fetch("square_with_form").to_f ** 2
-  
-  erb(:square_results)
-end
-
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
@@ -25,11 +13,23 @@ get("/") do
   
 end
 
-get("/square/new") do
-  
-
-  erb(:square)
+get("/results") do
+  erb(:results)
 end
+
+get("/square/new") do
+    erb(:square)
+end
+
+get("/square/results") do
+  @the_num = params.fetch("square_with_form").to_f
+  @the_result = params.fetch("square_with_form").to_f ** 2
+  erb(:square_results)
+end
+
+
+
+
 
 get("square-root/new") do
 
