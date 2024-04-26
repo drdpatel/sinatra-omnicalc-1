104cd9cea06fedadb264c27c04f344d137e48172
drdpatel <165212603+drdpatel@users.noreply.github.com>
2024-04-26 20:50:11 +0000

    home page

--- app.rb
+++ app.rb
@@ -6,11 +6,7 @@ erb(:hello)
 end
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
-  
+  erb(:square)
 end
 
 get("/results") do
@@ -18,7 +14,7 @@ get("/results") do
 end
 
 get("/square/new") do
-    erb(:square)
+  
 end
 
 get("/square/results") do
